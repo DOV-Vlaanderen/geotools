e794d1b239da8191ae161ef29f682c058134fd27
Niels Charlier <niels@scitus.be>
2019-03-03 10:02:40 +0100

    Revert "GEOS-9113 list functions"
    This reverts commit 19d1d5544bdc29cb5135b72ee3e7f4530fad0d18.

--- modules/library/main/src/main/java/org/geotools/filter/function/FilterFunction_litem.java
+++ modules/library/main/src/main/java/org/geotools/filter/function/FilterFunction_litem.java
@@ -1,24 +1,9 @@
-/*
- *    GeoTools - The Open Source Java GIS Toolkit
- *    http://geotools.org
- *
- *    (C) 2019, Open Source Geospatial Foundation (OSGeo)
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- */
 package org.geotools.filter.function;
 
 import static org.geotools.filter.capability.FunctionNameImpl.parameter;
 
 import java.util.List;
+
 import org.geotools.filter.FunctionExpressionImpl;
 import org.geotools.filter.capability.FunctionNameImpl;
 import org.opengis.filter.capability.FunctionName;
