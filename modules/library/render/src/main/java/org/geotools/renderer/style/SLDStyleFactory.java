e0356d483ee90e6951e61778743efc27bbf5367f
Niels Charlier <niels@scitus.be>
2019-10-15 17:04:42 +0200

    GEOT-6404 sld style: mark doesn't work without size (regression)

--- modules/library/render/src/main/java/org/geotools/renderer/style/SLDStyleFactory.java
+++ modules/library/render/src/main/java/org/geotools/renderer/style/SLDStyleFactory.java
@@ -1687,7 +1687,7 @@ public class SLDStyleFactory {
             return fallback;
         }
         Double d = exp.evaluate(f, Double.class);
-        if (d != null) {
+        if (d != null && !Double.isNaN(d)) {
             return d.doubleValue();
         }
         return fallback;
