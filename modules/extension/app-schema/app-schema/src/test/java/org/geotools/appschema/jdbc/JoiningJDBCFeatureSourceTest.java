94628cd3ba5196fcf444b3bf1e7ba97a89d55727
Niels Charlier <niels@scitus.be>
2021-02-15 17:01:27 +0100

    GEOT-6801 App-schema feature chaining (with joining) fails if you use multiple columns in your feature ID mapping

--- modules/extension/app-schema/app-schema/src/test/java/org/geotools/appschema/jdbc/JoiningJDBCFeatureSourceTest.java
+++ modules/extension/app-schema/app-schema/src/test/java/org/geotools/appschema/jdbc/JoiningJDBCFeatureSourceTest.java
@@ -21,7 +21,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.Collections;
-
 import org.easymock.EasyMock;
 import org.geotools.data.joining.JoiningQuery;
 import org.geotools.data.postgis.PostGISDialect;
@@ -40,38 +39,38 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(JDBCDataStore.class)
 public class JoiningJDBCFeatureSourceTest {
-    
+
     @Test
     public void testMultipleIds() throws Exception {
         SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
         builder.setName("test");
         SimpleFeatureType origType = builder.buildFeatureType();
-        
-        JoiningQuery query =  new JoiningQuery();
+
+        JoiningQuery query = new JoiningQuery();
         JoiningQuery.QueryJoin join = new JoiningQuery.QueryJoin();
         join.getIds().add("one");
         join.getIds().add("two");
         query.setQueryJoins(Collections.singletonList(join));
-                
+
         JDBCDataStore mockStore = PowerMock.createNiceMock(JDBCDataStore.class);
         ContentEntry mockEntry = PowerMock.createNiceMock(ContentEntry.class);
-        PowerMock.expectPrivate(mockStore, 
-                JDBCDataStore.class.getMethod("getPrimaryKey", SimpleFeatureType.class), 
-                origType)
-            .andReturn(new PrimaryKey(null, Collections.emptyList()))
-            .anyTimes();
+        PowerMock.expectPrivate(
+                        mockStore,
+                        JDBCDataStore.class.getMethod("getPrimaryKey", SimpleFeatureType.class),
+                        origType)
+                .andReturn(new PrimaryKey(null, Collections.emptyList()))
+                .anyTimes();
         EasyMock.expect(mockStore.getSQLDialect())
-            .andReturn(new PostGISDialect(mockStore))
-            .anyTimes();
+                .andReturn(new PostGISDialect(mockStore))
+                .anyTimes();
         EasyMock.replay(mockStore);
         EasyMock.expect(mockEntry.getDataStore()).andReturn(mockStore).anyTimes();
         EasyMock.replay(mockEntry);
-        JoiningJDBCFeatureSource source = new JoiningJDBCFeatureSource(
-                new JDBCFeatureSource(mockEntry, null));
+        JoiningJDBCFeatureSource source =
+                new JoiningJDBCFeatureSource(new JDBCFeatureSource(mockEntry, null));
         SimpleFeatureType type = source.getFeatureType(origType, query);
         assertNotNull(type);
         assertEquals("FOREIGN_ID_0_0", type.getDescriptor(0).getName().getLocalPart());
         assertEquals("FOREIGN_ID_0_1", type.getDescriptor(1).getName().getLocalPart());
     }
-
 }
